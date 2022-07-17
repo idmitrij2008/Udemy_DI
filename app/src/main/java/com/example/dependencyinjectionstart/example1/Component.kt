f3169eb9c181422c54ee58b9a86b34ecb6702295
Журба Дмитрий <dmitriy.zhurba@gazprombank.ru>
2022-07-17 11:40:20 +0300

    lesson_02

---
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Component.kt
@@ -0,0 +1,22 @@
+package com.example.dependencyinjectionstart.example1
+
+class Component {
+
+    fun inject(activity: Activity) {
+        activity.computer = getComputer()
+        activity.keyboard = Keyboard()
+    }
+
+    private fun getComputer(): Computer {
+        val monitor = Monitor()
+        val keyboard = Keyboard()
+        val mouse = Mouse()
+        val computerTower = ComputerTower(
+            Storage(),
+            Memory(),
+            Processor()
+        )
+
+        return Computer(monitor, computerTower, keyboard, mouse)
+    }
+}
