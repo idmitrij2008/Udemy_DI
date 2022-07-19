0a0a706db919ecc22797751c7b1e0c5d8ebbe74a
Журба Дмитрий <dmitriy.zhurba@gazprombank.ru>
2022-07-19 21:03:20 +0300

    lesson_08

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
@@ -1,16 +1,29 @@
 package com.example.dependencyinjectionstart.example2.di
 
+import android.content.Context
 import com.example.dependencyinjectionstart.example2.presentation.MainActivity
+import dagger.BindsInstance
 import dagger.Component
 
 @Component(
     modules = [
         DataModule::class,
         DomainModule::class,
-        ContextModule::class
     ]
 )
 interface ApplicationComponent {
+
+    @Component.Builder
+    interface ApplicationComponentBuilder {
+
+        @BindsInstance
+        fun context(context: Context): ApplicationComponentBuilder
+
+        @BindsInstance
+        fun currentTime(currentTime: Long): ApplicationComponentBuilder
+
+        fun build(): ApplicationComponent
+    }
 
     fun inject(activity: MainActivity)
 }
